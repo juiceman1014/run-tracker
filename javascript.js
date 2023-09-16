c37df72d5c2535dca02ed1da8bfccfa4f5f827fa
juiceman1014 <justintran1014@gmail.com>
2023-09-16 16:14:34 -0700

    completed function that will display info on the individual run cards

--- javascript.js
+++ javascript.js
@@ -6,3 +6,22 @@ function Run(date, distance, rpe, pr){
     this.rpe = rpe,
     this.pr = pr
 }
+
+function displayRun(){
+    bodyContainer.innerHtml = "";
+    for (let i = 0; i < myRuns.length; i++){
+        const run = myRuns[i];
+
+        const runDiv = document.createElement("div");
+        runDiv.classList.add("run-card");
+        runDiv.innerHtml = `
+        <p>Date: ${run.date}</p>
+        <p>Distance: ${run.distance}</p>
+        <p>RPE: ${run.rpe}</p>
+        <button class = "remove-btn" data-book-id="${i}">Remove Book</button>
+        <input type="checkbox" class="pr-checkbox" data-book-id="${i}" ${run.pr ? 'checked' : ''}> PR
+        `
+    
+        runContainer.appendChild(runDiv);
+    }
+}
